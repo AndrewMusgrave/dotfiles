dbf95b3d9e617519a9ca15aef6c7c958687a35ea
Andrew Musgrave <andrew.musgrave@shopify.com>
2022-06-17 11:42:53 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,12 @@ if [[ "$SPIN" = 1 ]]
 then
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    cartridge insert AndrewMusgrave/orders-team-bootstrap
-    . /cartridges/orders-team-bootstrap/setup.sh
+    cartridge insert AndrewMusgrave/order-tasks-bootstrap
+    . /cartridges/order-tasks-bootstrap/setup.sh
 
     cartridge insert shameelabd/shopify-reportify-config
     . /cartridges/shopify-reportify-config/setup.sh
-﻿
+
     restart
   fi
 fi
